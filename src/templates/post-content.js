a4c333edc4d19f32b1f886922ce03f11a7b1d875
Julian Marmier <julianmarmier@gmail.com>
2022-08-03 17:17:07 +0200

    Mobile fixes

--- src/templates/post-content.js
+++ src/templates/post-content.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import * as styles from "../style/Post.module.scss"
 
 import useWindowDimensions from "../helper/windowDimensions"
-
 import Lightbox from "../components/lightbox"
 
 const PostContent = (props) => {
@@ -15,33 +14,33 @@ const PostContent = (props) => {
   const WIN_BREAKPOINT = 740
 
   return width < WIN_BREAKPOINT ? (
-    <div className={`${styles.gridMobile} ${showMedia ? styles.active : ""}`}>
-      <div className={`${styles.gridLeft} grid-left-item scroller`}>
-        <section
-          className="art-main"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-      </div>
-      <div
-        className={`${styles.divider} ${showMedia ? styles.active : ""}`}
-        onClick={toggleShowMedia}
-      >
-        {showMedia ? "↓ Description" : "↑ Images"}
-      </div>
-      <div className={`${styles.gridRight} scroller`}>
-        {
-          <Lightbox
-            images={post.frontmatter.images}
-            videos={post.frontmatter.videos}
+      <div className={`${styles.gridMobile} ${showMedia ? styles.active : ""}`}>
+        <div className={`${styles.gridLeft} grid-left-item scroller`}>
+          <section
+            className="art-main"
+            dangerouslySetInnerHTML={{ __html: post.html }}
           />
-          /* post.frontmatter.images.map((img) => {
+        </div>
+        <div
+          className={`${styles.divider} ${showMedia ? styles.active : ""}`}
+          onClick={toggleShowMedia}
+        >
+          {showMedia ? "↓ Description" : "↑ Images"}
+        </div>
+        <div className={`${styles.gridRight} scroller`}>
+          {
+            <Lightbox
+              images={post.frontmatter.images}
+              videos={post.frontmatter.videos}
+            />
+            /* post.frontmatter.images.map((img) => {
                 // img.alt - image alt text
                 // img.image – Gatsby Image
                 // idea – inside <Lightbox/> component include both an inline version as well as a fullscreen version, 
               }) */
-        }
+          }
+        </div>
       </div>
-    </div>
   ) : (
     <div className={`${styles.gridTwoCol}`}>
       <div className={`${styles.gridLeft} grid-left-item scroller`}>
